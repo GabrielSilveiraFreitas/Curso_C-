8bdd69e277be80e9e80d5a84bf190f205c42b028
Gabriel <you@example.com>
2021-09-21 17:44:17 -0300

    Metodo Parse

---
+++ MetodoParse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MetodoParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txtNumero = "1985";
+
+            int numero = int.Parse(txtNumero);
+            byte num1 = byte.Parse("120");
+            double num2 = double.Parse("125623,57");
+            float num3 = float.Parse("457,75");
+
+            Console.WriteLine(num3);
+            
+        }
+    }
+}
