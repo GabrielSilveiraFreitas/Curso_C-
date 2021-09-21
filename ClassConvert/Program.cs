722c57dc128db78e0c5cf5b9ebadb2965df2b2b6
Gabriel <you@example.com>
2021-09-21 18:41:28 -0300

    ClassConvert

---
+++ ClassConvert/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = Convert.ToString(2500);
+            double num1 = Convert.ToDouble(false);
+            int num2 = Convert.ToInt32('C');
+            Console.WriteLine(num1);
+        }
+    }
+}
